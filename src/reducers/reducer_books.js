9ff0e0c4e243a21d87b0084956f3b04b9cc305e2
Aida Real <areal060781@gmail.com>
2018-07-13 23:17:00 -0500

    Containers - connecting redux to react

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: 'Javascript: The Good parts'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark tower'},
+        {title: 'Lord of the rings'}
+    ]
+}
