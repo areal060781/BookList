948566fba5f228967c6d32f8edd4cb0e506b7a7b
Aida Real <areal060781@gmail.com>
2018-07-14 10:40:30 -0500

    Conditional rendering

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
     return [
-        {title: 'Javascript: The Good parts'},
-        {title: 'Harry Potter'},
-        {title: 'The Dark tower'},
-        {title: 'Lord of the rings'}
+        {title: 'Javascript: The Good parts', pages: 101},
+        {title: 'Harry Potter', pages: 181},
+        {title: 'The Dark tower', pages: 1801},
+        {title: 'Lord of the rings', pages: 1015}
     ]
 }
